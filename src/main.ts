40a31000cdc787069efdfad948454f7cb49083dc
Ve4er <terew411@gmail.com>
2025-05-20 08:53:45 +0300

    dev v0.0.3

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import {NestFactory} from "@nestjs/core";
-import {AppModule} from "./app.module";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {JwtAuthGuard} from "./auth/jwt-auth.guard";
-import {ValidationPipe} from "./pipes/validation.pipe";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { ValidationPipe } from "./pipes/validation.pipe";
+import helmet from "helmet";
 
 
 async function start() {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule)
+    app.enableCors();
+
 
     const config = new DocumentBuilder()
         .setTitle('Урок по продвинотому BACKEND')
@@ -19,6 +22,8 @@ async function start() {
     SwaggerModule.setup('/api/docs', app, document)
 
     app.useGlobalPipes(new ValidationPipe())
+    app.use(helmet())
+
 
     await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
